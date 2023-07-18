6b70efc522ef1831ab0568847e9841308c1edfd7
AkinpejuRasheed <akinpejukorede6@gmail.com>
2023-07-18 23:39:46 +0800

    added header component

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import { GlobalStyle } from './GlobalStyle';
+import Header from './component/Header';
+
+function App() {
+  return ( 
+    <>
+
+   < Header />
+   <GlobalStyle/>
+    </>
+    
+   
+  );
+
+}
+
+
+
+export default App;
+
+
